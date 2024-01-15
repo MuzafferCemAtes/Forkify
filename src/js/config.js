f253f30fea6dca4d089d9258ecdc5395ea294361
Muzaffer Cem Ates <atesm@sheridancollege.ca>
2024-01-14 19:36:49 -0500

    initialize Forkify

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+// export const KEY = '<YOUR_KEY>';
+export const KEY = 'cf8eb2a6-0f37-4890-9897-e146ba494994';
+export const MODAL_CLOSE_SEC = 2.5;
